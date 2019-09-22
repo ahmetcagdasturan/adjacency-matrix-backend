b14cecf6de4c2ccb355c2b2a6e757853ceb1684c
Ahmet Çağdaş Turan <ahmetcagdasturan@outlook.com>
2019-09-22 21:24:46 +0300

    max n/2 rule added.

--- api/Services/MatrixService.js
+++ api/Services/MatrixService.js
@@ -1,31 +1,22 @@
 class MatrixService{
 
     createRandomAdjacencyMatrix(param) {
-        var array = this.create2DArrayWithOnes(param);
-        var randValue = 0;
-        var totalZeroInOneNode = 0;
+        var array = this.create2DArrayWithZeros(param);
+        var nodeRelationCounts = [];
+        var maxRelationCount = Math.floor(param/2);
+
+        for(var m=0;m<param;m++){
+            nodeRelationCounts.push(0);
+        }    
 		for(var i=0;i<param;i++) {
-            totalZeroInOneNode = 0;
-			for(var j=0;j<param;j++) {
-                if(j>i){
-                    if(totalZeroInOneNode >= Math.round((param-1)/2)){
-                        break;
-                    }
-                    randValue = Math.round(Math.random());
-                    totalZeroInOneNode = randValue == 0 ? totalZeroInOneNode + 1 : totalZeroInOneNode;
-                    var zeros = 0;
-                    for(var k=0; k<param; k++){
-                        if(array[j][k]==0){
-                            zeros += 1;
-                        }
-                    }
-                    randValue = zeros >= Math.round((param-1)/2) ? 1 : randValue;
+			for(var j=0;j<i;j++) {
+                if(nodeRelationCounts[i]< maxRelationCount && nodeRelationCounts[j]<maxRelationCount){
+                    var randValue = Math.round(Math.random());
                     array[i][j] = randValue;
                     array[j][i] = randValue;
-                    
-                }else{
-                    if(array[i][j]==0){
-                        totalZeroInOneNode +=1
+                    if(randValue==1){
+                       nodeRelationCounts[i]++;
+                       nodeRelationCounts[j]++;
                     }
                 }
             }
@@ -34,21 +25,16 @@ class MatrixService{
         return array;
     }
 
-    create2DArrayWithOnes(rows) {
+    create2DArrayWithZeros(rows) {
         var arr = [];
         for(var i = 0; i < rows; ++i) {
             arr.push([]);
             for(var j = 0; j < rows; ++j) {
-                if(i==j){
-                    arr[i].push(0); 
-                }else{
-                    arr[i].push(1);
-                }
+                arr[i].push(0); 
             }
         }
         return arr;
     }
-
     createEmpty2DArray(rows) {
         var arr = [];
         for(var i = 0; i < rows; ++i) {
